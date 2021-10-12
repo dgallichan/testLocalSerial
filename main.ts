a75b9f197bb7f8fd200e662cd53ee22f029765da
Daniel Gallichan <dgallichan@yahoo.com>
2021-10-12 15:53:30 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 basic.showIcon(IconNames.House)
 basic.forever(function () {
-    serial.writeValue("x", input.acceleration(Dimension.Strength))
-    basic.pause(100)
+    serial.writeValue("a.x", input.acceleration(Dimension.X))
+    serial.writeValue("a.y", input.acceleration(Dimension.Y))
+    serial.writeValue("a.z", input.acceleration(Dimension.Z))
+    basic.pause(5)
 })
