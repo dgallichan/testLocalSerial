f42d69015a6f46c66202cfe5e78d5e39081055cd
Daniel Gallichan <dgallichan@yahoo.com>
2023-02-20 09:59:07 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,5 +3,8 @@ basic.forever(function () {
     serial.writeValue("a.x", input.acceleration(Dimension.X))
     serial.writeValue("a.y", input.acceleration(Dimension.Y))
     serial.writeValue("a.z", input.acceleration(Dimension.Z))
+    serial.writeValue("m.x", input.magneticForce(Dimension.X))
+    serial.writeValue("m.y", input.magneticForce(Dimension.Y))
+    serial.writeValue("m.z", input.magneticForce(Dimension.Z))
     basic.pause(5)
 })
