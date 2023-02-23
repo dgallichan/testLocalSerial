20d3f7e425ea2c62c062f7180edd03ec85618106
Daniel Gallichan <dgallichan@yahoo.com>
2023-02-23 15:30:13 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,21 @@
-basic.showIcon(IconNames.House)
+input.onButtonPressed(Button.A, function () {
+    isLogging = !(isLogging)
+})
+control.onEvent(EventBusSource.MICROBIT_ID_ACCELEROMETER, EventBusValue.MICROBIT_ACCELEROMETER_EVT_DATA_UPDATE, function () {
+    if (isLogging) {
+        serial.writeLine("" + convertToText(control.millis()) + "," + convertToText(input.acceleration(Dimension.X)) + "," + convertToText(input.acceleration(Dimension.Y)) + "," + convertToText(input.acceleration(Dimension.Z)))
+        led.toggle(0, 0)
+    }
+})
+let isLogging = false
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    # . . . #
+    # . . . #
+    # # # # #
+    `)
+isLogging = false
 basic.forever(function () {
-    serial.writeLine("" + convertToText(input.acceleration(Dimension.X)) + "," + convertToText(input.acceleration(Dimension.Y)) + "," + convertToText(input.acceleration(Dimension.Z)))
+	
 })
